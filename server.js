b7d0055a9df8e500ae879a2fec19a0546c8bc9dd
Francyl Koudanou <devartemis81@gmail.com>
2025-07-30 18:56:57 +0100

    Enfin

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const db = require('./db'); // Assure-toi que le chemin est correct
+const db = require('./server/config/db');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
